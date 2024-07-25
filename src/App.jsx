d0174b005199b755a3d25b776b899ffa41f80675
Lamia Fatiha <f6lamia@gmail.com>
2024-07-25 20:11:30 +0600

    navbar contents added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Nav from "./components/Nav"
 
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <Nav />
+    </>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import logo from '../assets/image/logo1.png'
 const Nav = () => {
   return (
     <div>
+      <div>
+        <span>e-wallet</span>
+        <img src={logo} alt="logo" />
+      </div>
+      <nav>
+        <ul>
+            <li><a href="#">services</a></li>
+            <li><a href="#">Business</a></li>
+            <li><a href="#">Help</a></li>
+            <li><a href="#">career</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">blog</a></li>
+        </ul>
+        <button>e-wallet App</button>
+        <span> En | Bn</span>
+      </nav>
       
     </div>
   )
