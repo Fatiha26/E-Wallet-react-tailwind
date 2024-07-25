19d57460638fe045bea76c914cd188d629b5ead0
Lamia Fatiha <f6lamia@gmail.com>
2024-07-25 20:18:52 +0600

    navbar styling on going

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,23 +1,23 @@
 import logo from '../assets/image/logo1.png'
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <span>e-wallet</span>
-        <img src={logo} alt="logo" />
+    <div className='flex items-center justify-around py-4'>
+      <div className='flex items-center'>
+        <span className='text-2xl font-mono'>e-wallet</span>
+        <img src={logo} alt="logo" className='w-12' />
       </div>
       <nav>
-        <ul>
-            <li><a href="#">services</a></li>
-            <li><a href="#">Business</a></li>
-            <li><a href="#">Help</a></li>
-            <li><a href="#">career</a></li>
-            <li><a href="#">about</a></li>
-            <li><a href="#">blog</a></li>
+        <ul className='flex justify-evenly capitalize'>
+            <li><a href="#" className='py-4 px-8'>services</a></li>
+            <li><a href="#" className='py-4 px-8'>Business</a></li>
+            <li><a href="#" className='py-4 px-8'>Help</a></li>
+            <li><a href="#" className='py-4 px-8'>career</a></li>
+            <li><a href="#" className='py-4 px-8'>about</a></li>
+            <li><a href="#" className='py-4 px-8'>blog</a></li>
         </ul>
-        <button>e-wallet App</button>
-        <span> En | Bn</span>
       </nav>
+      <button>e-wallet App</button>
+      <span> En | Bn</span>
       
     </div>
   )
