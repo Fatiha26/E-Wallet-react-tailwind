295707fe5c816854cb1fccc1f4a6f4a56a6c5cb4
Lamia Fatiha <f6lamia@gmail.com>
2024-07-26 12:06:31 +0600

    button component added

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+
+const Button = () => {
+  return (
+    <>
+     <button className='py-2 px-6 rounded-3xl text-md border border-white font-bold shadow'>e-wallet App</button>
+     <span className='text-md'> En | Bn</span>    
+    </>
+  )
+}
+
+export default Button;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/image/logo1.png';
 import NavLinks from './NavLinks';
+import Button from './Button';
 const Navbar = () => {
   return (
     <>
@@ -9,13 +10,14 @@ const Navbar = () => {
         <span className='text-lg font-mono'>e-wallet</span>
         <img src={logo} alt="logo" className='w-10' />
       </div>
-      <ul className='flex justify-evenly capitalize'>
+      <ul className='md:flex hidden justify-evenly capitalize'>
             <li>
               <Link to="/" className='py-2 px-6 text-md border-2 border-transparent hover:border-white transition delay-150 mr-4 border-dotted rounded-3xl'>
                 services
               </Link>
             </li>   
-            <NavLinks />     
+            <NavLinks />
+            <Button />     
       </ul>      
     </nav>
     </>
