2d1fe7b5270322d2126c8a74dc0cd60522a68781
Lamia Fatiha <f6lamia@gmail.com>
2024-07-28 12:10:26 +0600

    button color changed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 const Button = () => {
   return (
     <>
-     <button className='py-2 px-6 rounded-3xl text-md border text-black border-white font-bold shadow-md shadow-gray-500 text-purple-700'>e-wallet App</button>
+     <button className='py-2 px-6 rounded-3xl text-md border  border-white font-bold shadow-md shadow-gray-500 text-purple-700'>e-wallet App</button>
      <span className='text-md font-bold ml-8 text-purple-700'> En | Bn</span>    
     </>
   )
