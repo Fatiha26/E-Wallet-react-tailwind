30e675ac9c081a2871ac07985927f90a22a2d460
Lamia Fatiha <f6lamia@gmail.com>
2024-07-28 10:26:10 +0600

    button shadow added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 const Button = () => {
   return (
     <>
-     <button className='py-2 px-6 rounded-3xl text-md border text-orange-500 border-white font-bold shadow'>e-wallet App</button>
+     <button className='py-2 px-6 rounded-3xl text-md border text-black border-white font-bold shadow-md shadow-gray-500 bg-orange-300'>e-wallet App</button>
      <span className='text-md font-bold ml-8 text-orange-500'> En | Bn</span>    
     </>
   )
