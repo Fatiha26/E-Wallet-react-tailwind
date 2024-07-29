ce4637b2414d5803acf7b6aa9552e1da87dd24a7
Lamia Fatiha <f6lamia@gmail.com>
2024-07-29 20:51:01 +0600

    platform section grid done

--- src/components/Platforms.jsx
+++ src/components/Platforms.jsx
@@ -1,12 +1,12 @@
 import { platforms } from '../constants/data'
 const Platforms = () => {
   return (
-    <div>
+    <div className='my-8 bg-purple-100 py-12'>
        <h1 className='text-center text-5xl font-semibold'>One Platform for all Financial Solutions</h1>
-       <div>
+       <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-12 py-12'>
           {platforms.map((item,index)=>(
             <div key={index}>
-             <img src={item.img} alt="" />
+             <img src={item.img} alt="img" />
              <h3>{item.heading}</h3>
              <p>{item.text}</p>
              <a href="#">{item.btn}</a>
