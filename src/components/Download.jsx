aaa53cda4cf6e0394aa0a6cda9e5f207c5bfa729
Lamia Fatiha <f6lamia@gmail.com>
2024-07-30 22:15:26 +0600

    download section styling done

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -7,24 +7,24 @@ import apple from '../assets/image/apple.png'
 const Download = () => {
   return (
     <>
-     <div className="w-full"
+     <div className="w-full flex justify-center items-center gap-24"
       style={{
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url(${bgImg})`,
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.6)), url(${bgImg})`,
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         height: '100vh' 
       }}>
 
-        <div>
-           <h3>Download e-wallet App</h3>
-           <p>Open your Account from the e-wallet App in a few simple steps and get up to 125 TK bonus!</p>
-           <div>
-             <img src={google} alt="google" />
-             <img src={apple} alt="apple" />        
+        <div className='text-white'>
+           <h3 className='text-4xl font-bold'>Download e-wallet App</h3>
+           <p className='text-xl mt-4'>Open your Account from the e-wallet App in a few simple steps and <br/> get up to 125 TK bonus!</p>
+           <div className='flex items-center mt-4'>
+             <img src={google} alt="google"  className='h-20'/>
+             <img src={apple} alt="apple"  className='h-14'/>        
            </div>
         </div>
-        <div>
-           <img src={app} alt="app" />
+        <div >
+           <img src={app} alt="app" className='w-[300px] h-[520px]' />
         </div>
      </div>
       
