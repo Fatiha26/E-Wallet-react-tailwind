0330800bd385b1e5b924bad42a83c1ec68518d09
Lamia Fatiha <f6lamia@gmail.com>
2024-07-30 21:37:40 +0600

    starting download section

--- src/components/Platforms.jsx
+++ src/components/Platforms.jsx
@@ -9,9 +9,9 @@ const Platforms = () => {
             <div key={index} className='bg-white shadow-md rounded-xl shadow-gray-400 hover:shadow-xl cursor-pointer text-center'>
              <img src={item.img} alt="img" className='w-full h-52 rounded-2xl'/>
             <div className='p-8'>
-             <h3 className='text-3xl hover:text-purple-500 mb-4'>{item.heading}</h3>
+             <h3 className='text-3xl hover:text-purple-700 mb-4'>{item.heading}</h3>
              <p className='text-md text-gray-500 mb-8'>{item.text}</p>
-             <a href="#" className='text-purple-500'>{item.btn}</a>
+             <a href="#" className='text-purple-700'>{item.btn}</a>
             </div>
           </div>
           ))}
