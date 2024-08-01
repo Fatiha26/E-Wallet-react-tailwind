6570f4367aaf67b38e1c0011d0de75342f9ed257
Lamia Fatiha <f6lamia@gmail.com>
2024-08-01 11:44:13 +0600

    business container subheading

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,9 +2,11 @@
 const Business = () => {
   return (
     <>
-     <div className="py-16">
+     <div className="py-16 px-28">
      <h1 className='text-center text-3xl md:text-5xl font-semibold'>e-wallet for Business</h1>
-     <p></p>
+     <p className="text-center text-md md:text-lg mt-6">The diverse range of financial solutions offered by e-wallet ensures fast, easy and safe<br/>
+        transactions for your business. Make your day-to-day operations more efficient with e-wallet.
+     </p>
         <div>
             <div></div>
             <div></div>
