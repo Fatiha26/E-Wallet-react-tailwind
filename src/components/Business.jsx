d6ce0a8c58c862e6939a7d06aca22126d8709ef7
Lamia Fatiha <f6lamia@gmail.com>
2024-08-01 11:45:20 +0600

    business container logo color

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,7 +3,7 @@ const Business = () => {
   return (
     <>
      <div className="py-16 px-28">
-     <h1 className='text-center text-3xl md:text-5xl font-semibold'>e-wallet for Business</h1>
+     <h1 className='text-center text-3xl md:text-5xl font-semibold'><span className="text-purple-700">e-wallet</span> for Business</h1>
      <p className="text-center text-md md:text-lg mt-6">The diverse range of financial solutions offered by e-wallet ensures fast, easy and safe<br/>
         transactions for your business. Make your day-to-day operations more efficient with e-wallet.
      </p>
