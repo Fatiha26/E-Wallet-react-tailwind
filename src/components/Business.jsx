0a4d72c8f0dcd04d1c52452e494e352c521ce839
Lamia Fatiha <f6lamia@gmail.com>
2024-08-03 12:15:18 +0600

    content added

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -4,6 +4,7 @@ import { FaHouseChimney } from "react-icons/fa6";
 import { GiFamilyHouse } from "react-icons/gi";
 import { FaPrayingHands } from "react-icons/fa";
 import { IoIosArrowForward } from "react-icons/io";
+import bg2 from "../assets/image/bg2.png";
 
 const Business = () => {
   return (
@@ -14,8 +15,17 @@ const Business = () => {
         transactions for your business. Make your day-to-day operations more efficient with e-wallet.
      </p>
         <div>
-            <div></div>
-            <div></div>
+            <div>
+                <FaCartArrowDown />
+                <MdWork />
+                <FaHouseChimney />
+                <GiFamilyHouse />
+                <FaPrayingHands />
+                <IoIosArrowForward />
+            </div>
+            <div>
+               <img src={bg2} alt="" />
+            </div>
         </div>
      </div>
       
