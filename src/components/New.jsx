aa15527e65fe22d6c21a0aec8373e602e58bc8d3
Fatiha26 <f6lamia@gmail.com>
2024-08-15 21:01:10 +0600

    whats new padding

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,10 +2,10 @@
 const New = () => {
   return (
     <>
-     <section>
+     <section className="p-16">
        <h1 className='text-center text-3xl md:text-5xl font-semibold'>
         What&apos;s New in
-        <span className="text-purple-700">eWallet</span></h1>
+        <span className="text-purple-700"> eWallet</span></h1>
 
      </section>
 
