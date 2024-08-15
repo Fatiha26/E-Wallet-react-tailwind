fd3d4e30d34f3859be6c52a885150fb6889f3db4
Fatiha26 <f6lamia@gmail.com>
2024-08-15 20:59:22 +0600

    whats new heading

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -2,7 +2,13 @@
 const New = () => {
   return (
     <>
-        
+     <section>
+       <h1 className='text-center text-3xl md:text-5xl font-semibold'>
+        What&apos;s New in
+        <span className="text-purple-700">eWallet</span></h1>
+
+     </section>
+
     </>
   )
 }
