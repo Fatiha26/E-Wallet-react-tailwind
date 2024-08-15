4d9b1d3eb9a116aea5bfd93328d3ce586823ae5e
Fatiha26 <f6lamia@gmail.com>
2024-08-15 22:11:22 +0600

    whats new contents

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -8,16 +8,18 @@ const New = () => {
         What&apos;s New in
         <span className="text-purple-700"> eWallet</span>
       </h1>
-      <div className="grid grid-cols-3 gap-8 mt-12">
+      <div className="grid grid-cols-3 gap-8 mt-12 ">
         {
             news.map((item,index)=>(
-              <div key={index}>
+              <div key={index} className="shadow shadow-slate-400 mr-2">
                  <img src={item.img} alt="news img" />
-                 <h3>{item.heading}</h3>
-                 <div>
+                 <div className="px-2">
+                 <h3 className="mt-4 text-xl font-bold">{item.heading}</h3>
+                 <div className="flex items-center gap-2 mb-4 mt-4 text-slate-500">
                    <p>{item.text}</p>
                    <p>{item.line}</p>
                    <p>{item.read}</p>
+                 </div>
                  </div>
               </div>
             ))
