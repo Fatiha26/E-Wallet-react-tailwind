d16c78fa8f62b8e879bef2f61aaf0814cad9e4ff
Fatiha26 <f6lamia@gmail.com>
2024-08-15 22:12:31 +0600

    whats new done

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -11,10 +11,10 @@ const New = () => {
       <div className="grid grid-cols-3 gap-8 mt-12 ">
         {
             news.map((item,index)=>(
-              <div key={index} className="shadow shadow-slate-400 mr-2">
+              <div key={index} className="shadow shadow-slate-400 hover:shadow-slate-600 cursor-pointer mr-2">
                  <img src={item.img} alt="news img" />
                  <div className="px-2">
-                 <h3 className="mt-4 text-xl font-bold">{item.heading}</h3>
+                 <h3 className="mt-4 text-xl font-bold hover:text-purple-700">{item.heading}</h3>
                  <div className="flex items-center gap-2 mb-4 mt-4 text-slate-500">
                    <p>{item.text}</p>
                    <p>{item.line}</p>
