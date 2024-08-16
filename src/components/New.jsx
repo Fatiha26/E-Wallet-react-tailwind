805d29ab8d9160d4b6f5ca285a2ffdd9522ffaeb
Fatiha26 <f6lamia@gmail.com>
2024-08-16 23:12:53 +0600

    adding button

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -25,6 +25,7 @@ const New = () => {
             ))
         }
       </div>
+      <button className='py-2 px-6 rounded-3xl text-md border  border-white font-bold shadow-md shadow-gray-500 text-purple-700 hover:text-black'>ewallet App</button>
 
      </section>
 
