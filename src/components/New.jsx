8f22dcd0e1e13825f7823a9021f80b10941e2dc3
Fatiha26 <f6lamia@gmail.com>
2024-08-16 23:16:08 +0600

    button margin

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -25,7 +25,8 @@ const New = () => {
             ))
         }
       </div>
-      <button className='py-2 px-6 rounded-3xl text-md border  border-white font-bold shadow-md shadow-gray-500 text-purple-700 hover:text-black'>
+      <button className='py-3 px-6 rounded text-md border  border-white font-bold shadow-md 
+      shadow-gray-500 text-purple-700 hover:text-black mt-8'>
           Learn More
       </button>
 
