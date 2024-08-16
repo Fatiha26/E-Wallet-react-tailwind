9efb9ca27254025b3ac750d497ce095e82f39676
Fatiha26 <f6lamia@gmail.com>
2024-08-16 23:23:53 +0600

    nearest points title

--- src/components/Nearest.jsx
+++ src/components/Nearest.jsx
@@ -2,7 +2,14 @@
 const Nearest = () => {
   return (
     <>
-        
+    <section className="p-16">
+       <h1 className='text-center text-3xl md:text-5xl font-semibold'>
+        Nearest
+        <span className="text-purple-700"> eWallet</span>
+        Touchpoints
+      </h1>
+    </section>
+
     </>
   )
 }
