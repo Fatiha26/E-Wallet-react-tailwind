c56ffd7af857a0d1069b14e45f2c15e21a3d6849
Fatiha26 <f6lamia@gmail.com>
2024-08-16 23:14:51 +0600

    button txt

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -25,7 +25,9 @@ const New = () => {
             ))
         }
       </div>
-      <button className='py-2 px-6 rounded-3xl text-md border  border-white font-bold shadow-md shadow-gray-500 text-purple-700 hover:text-black'>ewallet App</button>
+      <button className='py-2 px-6 rounded-3xl text-md border  border-white font-bold shadow-md shadow-gray-500 text-purple-700 hover:text-black'>
+          Learn More
+      </button>
 
      </section>
 
