c8e08210a9022192dbf0c1a8ce57b13851428dcb
Fatiha26 <f6lamia@gmail.com>
2024-08-22 23:11:40 +0600

    secure heading

--- src/components/Secure.jsx
+++ src/components/Secure.jsx
@@ -3,7 +3,11 @@ const Secure = () => {
   return (
     <>
         <section>
-            
+        <h1 className='text-center text-3xl md:text-5xl font-semibold'>
+        Nearest
+        <span className="text-purple-700"> eWallet </span>
+         Touchpoints
+      </h1>
         </section>
     </>
   )
