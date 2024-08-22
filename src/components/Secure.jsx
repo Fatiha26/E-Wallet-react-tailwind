432cb418f4040386e450201ec6c408afdbff2bd1
Fatiha26 <f6lamia@gmail.com>
2024-08-22 23:12:49 +0600

    marging padding

--- src/components/Secure.jsx
+++ src/components/Secure.jsx
@@ -2,7 +2,7 @@
 const Secure = () => {
   return (
     <>
-        <section>
+        <section className="px-16 mt-8">
         <h1 className='text-center text-3xl md:text-5xl font-semibold'>
         Secure and Convenient Experience
       </h1>
