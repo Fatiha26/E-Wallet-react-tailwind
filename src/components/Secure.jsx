c3d737bf727f72a61723aa29aecead21d3821f16
Fatiha26 <f6lamia@gmail.com>
2024-08-28 21:05:43 +0600

    secure section

--- src/components/Secure.jsx
+++ src/components/Secure.jsx
@@ -6,6 +6,9 @@ const Secure = () => {
         <h1 className='text-center text-3xl md:text-5xl font-semibold'>
         Secure and Convenient Experience
       </h1>
+      <div >
+
+      </div>
         </section>
     </>
   )
